5e95ba4d15ce5771e72e5ee25e676832da0238bd
Ali Khaitov <ali20032508@email.com>
2025-10-19 22:38:34 +0500

    Changes

--- mytest/settings.py
+++ mytest/settings.py
@@ -71,18 +71,18 @@ WSGI_APPLICATION = 'mytest.wsgi.application'
 
 #PostgreSQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } 
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'buxoro_db',
-    #     'USER': 'buxoro_user',
-    #     'PASSWORD': 'Bahriddin0121',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # } 
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buxoro_db',
+        'USER': 'buxoro_user',
+        'PASSWORD': 'Bahriddin0121',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 # Валидаторы паролей
